83541b26f20185a8576028ab708e3bd42383040f
joevaas <133934323+joevaas@users.noreply.github.com>
2025-01-01 12:58:59 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,17 +20,17 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/in_Tamil_Anime'>Tᴇᴀᴍ 59 ( Iɴᴅᴇx ) 🔰</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/Tamil_Aime_Files'>Mᴏᴏɴ Wᴀʟᴋᴇʀ</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b> @in_Tamil_Anime"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
+<b>🛍 UPI ID:</b> <code>joevaasaaser@sbi</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
@@ -50,9 +50,9 @@ If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upt
 
 For Example :-
 
-<code>By @Madflix_Bots</code>
+<code>By @in_Tamil_Anime</code>
 
-💬 For Any Help Contact @MadflixOfficials
+💬 For Any Help Contact @Tamil_Aime_Files
 """
 
 
